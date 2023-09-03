a26557736c7a35cafb4b9520d582b3e9862a0177
trevor-tfe <108952876+trevor-tfe@users.noreply.github.com>
2023-09-03 15:46:25 -0500

    Module 2 EF Core Web App

---
+++ Mod2/EFCoreWebAppStewart/EFCoreWebAppStewart/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using EFCoreWebAppStewart.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+namespace EFCoreWebAppStewart.Controllers
+{
+    public class HomeController : Controller
+    {
+        private MovieContext context { get; set; }
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(MovieContext ctx, ILogger<HomeController> logger) { context = ctx; _logger = logger; }
+
+
+        public IActionResult Index()
+        {
+            var movies = context.Movies.Include(m => m.Genre).OrderBy(m => m.Name).ToList();
+            return View(movies);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Mod2/EFCoreWebAppStewart/EFCoreWebAppStewart/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EFCoreWebAppStewart.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
